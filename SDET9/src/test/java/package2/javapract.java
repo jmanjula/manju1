967f18bc48a08e652059ce9be5a3b60645149668
Manju <Manju@DESKTOP-50PIISQ>
2022-10-19 11:44:19 +0530

    javapract changes from unstaged to staged

---
+++ SDET9/src/test/java/package2/javapract.java
@@ -0,0 +1,25 @@
+package package2;
+
+import java.util.Arrays;
+
+public class javapract {
+
+	public static void main(String[] args) {
+		int[]a= {12,5,6,4,58,1};
+		for(int i=0;i<a.length;i++) {
+			for(int j=i+1;j<a.length;j++) {
+				
+				if(a[i]>a[j]) {
+					int temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+				
+			}
+		}
+     System.out.println(Arrays.toString(a));
+     
+     
+	}
+
+}
